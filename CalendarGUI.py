a91058b9b8c7ab7c65a562014de05bab3b25d4cb
Level140 <DanielZablocky@gmail.com>
2017-04-26 21:19:44 -0400

    Added updating Calendar
    Switched to intvars for month+year

--- CalendarGUI.py
+++ CalendarGUI.py
@@ -4,7 +4,14 @@ root = Tk()
 root.title("West Point Calendar")
 root.geometry('600x510')
 cally=[['0','0','0','0','0','0','0'],['0','0','0','0','0','0','0'],['0','0','0','0','0','0','0'],['0','0','0','0','0','0','0'],['0','0','0','0','0','0','0'],['0','0','0','0','0','0','0']]
-
+dayOnes=[]
+dayTwos=[]
+day=1
+cal=StringVar()
+month=IntVar()
+year=IntVar()
+year.set(2017)
+month.set(1)
 def click(event):
     co=0
     ro=0
@@ -23,7 +30,6 @@ def click(event):
         co=5
     if(x>420 and x<490):
         co=6
-        
     if(y>80 and y<130):
         ro=0
     if(y>130 and y<180):
@@ -36,38 +42,42 @@ def click(event):
         ro=4
     if(y>330 and y<380):
         ro=5
-    print cally[ro][co]
-    #print('{}, {}'.format(x, y))
-
+    print str(month.get())+"/"+str(cally[ro][co])+"/"+str(year.get())
+def createMonth(mn,yr):
+    str1 = calendar.month(year.get(), month.get())
+    cal.set(str1)
+    work=str1.split('\n')
+    nextWeek=0
+    #Get a grid of the calendar
+    for y in range(0,6):
+        for x in range(0,len(work[y+2])):
+            if(work[y+2][x]==' '):
+                pass
+            else:
+                if(work[y+2][x]=='0'):
+                    nextWeek+=1
+                    
+                if(nextWeek>0):
+                    cally[y][x/3]=str(nextWeek)+work[y+2][x]
+                else:
+                    cally[y][x/3]=work[y+2][x]
+def nextMonth():
+    if(month.get()==12):
+        month.set(1)
+        year.set(year.get()+1)
+    month.set(month.get()+1)
+    createMonth(month.get(),year.get())
 #Welcome Screen
 #/Welcome Screen
 
 #Calendar
-year = 2017
-month = 1
-str1 = calendar.month(year, month)
-work=str1.split('\n')
-print work
-nextWeek=0
-#Get a grid of the calendar
-for y in range(0,6):
-    for x in range(0,len(work[y+2])):
-        if(work[y+2][x]==' '):
-            pass
-        else:
-            if(work[y+2][x]=='0'):
-                nextWeek+=1
-                
-            if(nextWeek>0):
-                cally[y][x/3]=str(nextWeek)+work[y+2][x]
-            else:
-                cally[y][x/3]=work[y+2][x]
+createMonth(month,year)
 #/Get a grid of the calendar
+B = Button(root, text ="->", command = nextMonth)
+B.pack(anchor=E)
+label1 = Label(root, textvariable=cal, justify=LEFT, font=('courier', 30, 'bold'), bg='Blue')
+label1.pack(anchor=W,pady=10)
 
-for x in cally:
-    print x
-label1 = Label(root, text=str1, justify=LEFT, font=('courier', 30, 'bold'), bg='Blue')
-label1.pack(anchor=W,pady=50)
 #/Calendar
 
 label1.bind("<Button-1>", click)
